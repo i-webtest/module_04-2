032b1d1627970601aaf4123a0d0cb913df0849b5
i-webtest <stasbass80@gmail.com>
2023-04-07 12:15:12 +0300

    module_04 lesson_02

--- task_02.js
+++ task_02.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const result = [];
 let names;
 
 while ((names = prompt('Имя студента'))) {
+  result.push(names);
   if (names === null) {
     break;
   }
 
-  console.log(`${names}.`);
+  console.log(names);
 }
+
+console.log(`${result}.`);
